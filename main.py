a07b9367899c135e3c756a19073d65bbee9cb0fd
Alix Weaver <124537112+Alixstar11@users.noreply.github.com>
2023-07-17 18:48:22 +0100

    Update main.py
    Added bot setup code

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import discord
 import os
 import random
 from dotenv import load_dotenv
+
+token = os.getenv("token")
+intents = discord.Intents(messages=True, message_content=True)
+client = discord.Client()
+
